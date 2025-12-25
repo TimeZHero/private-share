3da544126f2792c3ce3ecdb1489f4d461ef301df
TimeZHero <67009812+TimeZHero@users.noreply.github.com>
2025-12-25 11:02:50 +0000

    fix code style

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
