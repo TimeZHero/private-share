547beaa22be9ce77c7e709201dc58f71662a2819
Stefano Fasoli <stefanofasoli17@gmail.com>
2025-12-25 12:01:59 +0100

    chore: add ESLint configuration and update package dependencies for improved code quality and linting support

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+    {
+        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr'],
+    },
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        files: ['**/*.{ts,tsx,js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {},
+    },
+]);
